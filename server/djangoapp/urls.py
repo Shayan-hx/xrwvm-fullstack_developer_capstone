0c816b3ab9620c8ea5ef93ba415e2d3d118e0598
Shayan-hx <Shayanali101@hotmail.com>
2024-08-27 20:29:15 -0400

    Added more Django stuff

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,26 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
-app_name = 'djangoapp'
+pp_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for logout
 
+    # path for registration
+   
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
+    path(route='login', view=views.login_user, name='login'),
+    # path for get_dealerships
+  
+  
     # path for dealer reviews view
 
     # path for add a review view
 
+    # path for reviews from dealer_id
+
+    # path for get_cars view
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
